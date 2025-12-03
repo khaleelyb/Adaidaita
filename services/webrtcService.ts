3bb19a86422f29ef924a73a9581e28962f0ae803
khaleelyb <ikhalil520@gmail.com>
2025-12-03 22:40:34 +0100

    webrtcService.ts

--- services/webrtcService.ts
+++ services/webrtcService.ts
@@ -1,5 +1,5 @@
 import { ICE_SERVERS } from '../constants';
-import { supabase } from './mockSupabase';
+import { supabase } from './supabase';
 
 export class WebRTCService {
   private peerConnection: RTCPeerConnection | null = null;
