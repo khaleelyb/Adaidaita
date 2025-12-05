572fd455b0c1f96a77d0e4b87233d2a772b80669
khaleelyb <ikhalil520@gmail.com>
2025-12-05 23:47:32 +0100

    supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -6,8 +6,6 @@ export const supabaseClient = createClient(SUPABASE_CONFIG.url, SUPABASE_CONFIG.
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true,
-    storage: window.localStorage,
-    storageKey: 'adaidaita-auth-token', // Consistent storage key across the app
+    detectSessionInUrl: true
   }
 });
